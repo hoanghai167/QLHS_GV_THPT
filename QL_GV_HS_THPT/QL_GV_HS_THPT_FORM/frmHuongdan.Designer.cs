3384f9597241c4c97dd8f9abcf558b9275ab0f28
NhuNam50 <32055881+NhuNam50@users.noreply.github.com>
2017-09-18 17:52:41 +0700

    Huong dan

--- QL_GV_HS_THPT/QL_GV_HS_THPT_FORM/frmHuongdan.Designer.cs
+++ QL_GV_HS_THPT/QL_GV_HS_THPT_FORM/frmHuongdan.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hướng dẫn sử dụng ứng dụng";
             // 
             // frmHuongdan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(635, 320);
+            this.Controls.Add(this.label1);
             this.Name = "frmHuongdan";
             this.Text = "frmHuongdan";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
